a625af972873c566a037137a7c91852d87ba7ce0
Andy Groome <groome.andy@gmail.com>
2021-01-03 23:27:03 +0000

    dot env support

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask
 import os
+import dotenv 
+import requests
+
+dotenv.load_dotenv('./.env')
+
+access_key = os.environ.get('ACCESS_KEY') 
+secret_key = os.environ.get('SECRET_KEY')
+
+auth_header = {'X-ApiKeys': f'accessKey={access_key}; secretKey={secret_key}'}
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "hello world!"
+def get_images(offset=0, limit=50):
+    url = 'https://cloud.tenable.com/container-security/api/v2/images' 
+    querystring = {'offset': offset, 'limit': limit} 
+    headers = {**{'Accept': 'application/json'}, **auth_header}
+    response = requests.request('GET', url, headers=headers, params=querystring)
+    return response.json()
+
+
+@app.route('/images')
+def list_images():
+    return get_images()
